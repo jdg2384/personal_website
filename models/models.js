f20ab0c7de4dc384422bc219fdadc8d9f15eec5d
Joseph Grounds <jgrounds@Josephs-MacBook-Pro.local>
2018-01-24 12:06:04 -0700

    first commit

---
+++ models/models.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt')
+const SALT_WORK_FACTOR = 10;
+
+const SiteShema = new Schema({
+    imageOne: { 
+        data: Buffer, 
+        contentType: String 
+    },
+    imageTwo: { 
+        data: Buffer, 
+        contentType: String 
+    },
+    imageThree: { 
+        data: Buffer, 
+        contentType: String 
+    },
+    description: String,
+    tech: String,
+
+});
+
+// const LoginShema = new Schema({
+//     username: { type: String, required: true, index: { unique: true } },
+//     password: { type: String, required: true }
+// });
+
+const Site = mongoose.model('Site', SiteShema);
+// const Login = mongoose.model('Login', LoginShema);
+module.exports = Site;
---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const pg = require('pg')
+const port = process.env.Port || 3010
+const app = express()
+const mongodb = require('mongodb')
+const mongoose = require('mongoose')
+const multer = require('multer')
+const bcrypt = require('bcrypt')
+// DATABASE CONNECTION
+const Site = require('./models/models.js')
+const Login = require('./models/models.js')
+mongoose.connect('mongodb://localhost/Sitedata')
+mongoose.connect('mongodb://localhost/Logindata')
+mongoose.Promise = global.Promise
+//Public
+app.use(express.static('public'))
+//Body-Parser
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+//Multer
+
+// Get All API
+app.get('/api',(req,res,next) => {
+    console.log(req.body)
+    res.send({working:'working'})
+    // Site.find((err, site) => {
+    //     res.json(site); // return all employees in JSON format
+    // })
+    // .catch(err => {
+    //     res.send(err)
+    // })
+})
+
+
+
+//Error
+app.use((err, req, res, next) => {
+    const status = err.status || 404
+    res.status(status).json({ error: err })
+})
+  
+app.use((req, res, next) => {
+    res.status(404).json({ error: { status: 404, message: 'Not found' }})
+})
+
+const listener = () => console.log( `Listening on port ${port}!`)
+app.listen(port, listener)
